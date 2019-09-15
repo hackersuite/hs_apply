c309ed2b39dd23fac6eb43430c7aae4a17773bdf
Sean Parker <sean.parker067@gmail.com>
2019-09-15 21:55:16 +0100

    Update inverify with new applicant naming

--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -2,14 +2,14 @@ import { Container } from "inversify";
 import { TYPES } from "./types";
 
 // Applicants
-import { IApplicationService, ApplicationService } from "./services";
-import { IApplicationController, ApplicationController } from "./controllers";
-import { ApplicationRepository } from "./repositories";
+import { IApplicantService, ApplicantService } from "./services";
+import { IApplicationController, ApplicationController, AdminController, IAdminController } from "./controllers";
+import { ApplicantRepository } from "./repositories";
 
 // Dashboard
 import { IDashboardController, DashboardController } from "./controllers";
 
-import { IRouter, ApplicationRouter, DashboardRouter } from "./routes";
+import { IRouter, ApplicationRouter, DashboardRouter, AdminRouter } from "./routes";
 import { ICache, Cache } from "./util/cache";
 
 const container = new Container();
@@ -17,14 +17,18 @@ const container = new Container();
 // Routers
 container.bind<IRouter>(TYPES.Router).to(ApplicationRouter);
 container.bind<IRouter>(TYPES.Router).to(DashboardRouter);
+container.bind<IRouter>(TYPES.Router).to(AdminRouter);
 
 // Applications
-container.bind<IApplicationService>(TYPES.ApplicationService).to(ApplicationService);
+container.bind<IApplicantService>(TYPES.ApplicantService).to(ApplicantService);
 container.bind<IApplicationController>(TYPES.ApplicationController).to(ApplicationController);
-container.bind<ApplicationRepository>(TYPES.ApplicationRepository).to(ApplicationRepository);
+container.bind<ApplicantRepository>(TYPES.ApplicantRepository).to(ApplicantRepository);
 
 // Dashboard
 container.bind<IDashboardController>(TYPES.DashboardController).to(DashboardController);
+
+// Admin
+container.bind<IAdminController>(TYPES.AdminController).to(AdminController);
 
 container.bind<ICache>(TYPES.Cache).toConstantValue(new Cache());
 
