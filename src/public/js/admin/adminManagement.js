7e8d6388e77b6382637b708bef2a35d6a8f81ebe
KhesimReid <khesim.reid@gmail.com>
2019-11-09 22:50:16 +0000

    Initial commit for branch.  Added  pagination code

--- src/public/js/admin/adminManagement.js
+++ src/public/js/admin/adminManagement.js
@@ -48,3 +48,63 @@ $(document).ready(function () {
     renderRows(elem.val());
   });
 });
+
+var current_page = 1;
+var records_per_page = 20;
+
+function prevPage()
+{
+    if (current_page > 1) {
+        current_page--;
+        changePage(current_page);
+    }
+}
+
+function nextPage()
+{
+    if (current_page < numPages()) {
+        current_page++;
+        changePage(current_page);
+    }
+}
+
+function changePage(page)
+{
+    var btn_next = document.getElementById("btn_next");
+    var btn_prev = document.getElementById("btn_prev");
+    var page_span = document.getElementById("page");
+
+    // Validate page
+    if (page < 1) page = 1;
+    if (page > numPages()) page = numPages();
+
+    const container = $(applicationsContainerID);
+    container.empty();
+
+    var applicantsOnPage = [];
+    for (var i = (page-1) * records_per_page; i < (page * records_per_page); i++) {
+      renderApplicationRow(container, allApplications[i]);
+    }
+
+    if (page == 1) {
+        btn_prev.style.visibility = "hidden";
+    } else {
+        btn_prev.style.visibility = "visible";
+    }
+
+    if (page == numPages()) {
+        btn_next.style.visibility = "hidden";
+    } else {
+        btn_next.style.visibility = "visible";
+    }
+}
+
+function numPages()
+{
+    return Math.ceil(allApplications.length / records_per_page);
+}
+
+window.onload = function() {
+    changePage(1);
+};
+
