8482bec3b6fde847f7b154f33a04abf229f425f9
Sean Parker <sean.parker067@gmail.com>
2020-02-25 01:59:07 +0000

    Fix javascript error when properties undefined Update css to show page number and space buttons evenly

--- src/public/js/admin/adminManagement.js
+++ src/public/js/admin/adminManagement.js
@@ -2,21 +2,24 @@
 var applicationsContainerID = "#applications-container";
 
 function renderApplicationRow(container, applicant) {
+  const na = "Not Provided";
+  const id = applicant && applicant["id"];
+
   var applicantRow = `
   <tr>
-    <td class="text-center">${applicant.name}</td>
-    <td class="text-center">${applicant.email}</td>
-    <td class="text-center">${applicant.university}</td>
-    <td class="text-center">${applicant.yearOfStudy}</td>
-    <td class="text-center">${applicant.applicationStatus}</td>
+    <td class="text-center">${applicant && applicant["name"] || na}</td>
+    <td class="text-center">${applicant && applicant["email"] || na}</td>
+    <td class="text-center">${applicant && applicant["university"] || na}</td>
+    <td class="text-center">${applicant && applicant["yearOfStudy"] || na}</td>
+    <td class="text-center">${applicant && applicant["applicationStatus"] || na}</td>
     <td class="td-actions text-center">
-      <a class="btn btn-primary" role="button" rel="tooltip" href="manage/${applicant.id}" target="_blank">
+      <a class="btn btn-primary" role="button" rel="tooltip" href="manage/${id}" target="_blank">
         <i class="material-icons">person</i>
       </a>
-      <button id="${applicant.id}-btn" class="btn btn-info" type="button" onclick="invite('${applicant.id}')">
+      <button id="${id}-btn" class="btn btn-info" type="button" onclick="invite('${id}')">
         <i class="material-icons">email</i>
       </button>
-      <button id="${applicant.id}-btn-checkin" class="btn btn-success" type="button" onclick="checkin('${applicant.id}')">
+      <button id="${id}-btn-checkin" class="btn btn-success" type="button" onclick="checkin('${id}')">
         <i class="material-icons">person_add</i>
       </button>
     </td>
@@ -113,61 +116,59 @@ $(document).ready(function () {
 });
 
 var current_page = 1;
-var records_per_page = 20;
+var records_per_page = 5;
 
-function prevPage()
-{
-    if (current_page > 1) {
-        current_page--;
-        changePage(current_page);
-    }
+function prevPage() {
+  if (current_page > 1) {
+    current_page--;
+    changePage(current_page);
+  }
 }
 
-function nextPage()
-{
-    if (current_page < numPages()) {
-        current_page++;
-        changePage(current_page);
-    }
+function nextPage() {
+  if (current_page < numPages()) {
+    current_page++;
+    changePage(current_page);
+  }
 }
 
-function changePage(page)
-{
-    var btn_next = document.getElementById("btn_next");
-    var btn_prev = document.getElementById("btn_prev");
-    var page_span = document.getElementById("page");
+function changePage(page) {
+  var btn_next = document.getElementById("btn_next");
+  var btn_prev = document.getElementById("btn_prev");
+  var page_span = document.getElementById("page");
 
-    // Validate page
-    if (page < 1) page = 1;
-    if (page > numPages()) page = numPages();
+  // Validate page
+  if (page < 1) page = 1;
+  if (page > numPages()) page = numPages();
 
-    const container = $(applicationsContainerID);
-    container.empty();
+  const container = $(applicationsContainerID);
+  container.empty();
 
-    var applicantsOnPage = [];
-    for (var i = (page-1) * records_per_page; i < (page * records_per_page); i++) {
-      renderApplicationRow(container, allApplications[i]);
-    }
+  var applicantsOnPage = [];
+  for (var i = (page-1) * records_per_page; i < Math.min(allApplications.length, page * records_per_page); i++) {
+    renderApplicationRow(container, allApplications[i]);
+  }
 
-    if (page == 1) {
-        btn_prev.style.visibility = "hidden";
-    } else {
-        btn_prev.style.visibility = "visible";
-    }
+  if (page == 1) {
+    btn_prev.style.visibility = "hidden";
+  } else {
+    btn_prev.style.visibility = "visible";
+  }
 
-    if (page == numPages()) {
-        btn_next.style.visibility = "hidden";
-    } else {
-        btn_next.style.visibility = "visible";
-    }
+  if (page == numPages()) {
+    btn_next.style.visibility = "hidden";
+  } else {
+    btn_next.style.visibility = "visible";
+  }
+
+  page_span.textContent = page;
 }
 
-function numPages()
-{
-    return Math.ceil(allApplications.length / records_per_page);
+function numPages() {
+  return Math.ceil(allApplications.length / records_per_page);
 }
 
 window.onload = function() {
-    changePage(1);
+  changePage(1);
 };
 
