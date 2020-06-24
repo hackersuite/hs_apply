71af7975a2b7d2582f96560043f35f6266ed1dea
Amish Shah <amishshah.2k@gmail.com>
2020-06-24 18:06:24 +0100

    chore: add basic typings for missing modules

--- src/@types/passport-cookie.d.ts
+++ src/@types/passport-cookie.d.ts
@@ -1,8 +1,3 @@
 declare module 'passport-cookie' {
-	import passport from "passport";
-	export default class CookieStrategy implements passport.Strategy {
-		name?: string;
-		authenticate(this: StrategyCreated<this>, req: express.Request, options?: any): any;
-		constructor (options: function|Record<string, any>, deserializeUser: function);
-	}
+	export default (options: function|Record<string, any>, deserializeUser: function) => any;
 }
